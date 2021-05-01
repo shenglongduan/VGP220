7aebf548676de03ca4fec16fc44c8ddc32281cd3
User <User@DESKTOP-QCM61S0>
2021-05-01 16:45:24 -0700

    Revert "111"
    This reverts commit 5950456a86c431714b3c84432294d4b6bd90df82.

---
+++ Assignment 3/Item.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <string>
+
+enum ItemType
+{
+	Weapon,
+	Shield,
+	Armor,
+	Potion,
+	Utility,
+	Charm,
+	Count
+};
+
+struct Item
+{
+	std::string name;
+	uint32_t quantity;
+	ItemType type;
+};
